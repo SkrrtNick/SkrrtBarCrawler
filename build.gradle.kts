727998d847f71762909f8506022bfe21aade4296
SkrrtNick <79165336+SkrrtNick@users.noreply.github.com>
2021-07-21 22:33:33 +0100

    Revert "Initial Commit"
    This reverts commit 405f87c2

--- build.gradle.kts
+++
@@ -1,8 +0,0 @@
-
-dependencies {
-    implementation(project(":libraries:fluffee"))
-    implementation(project(":libraries:rs-item-services"))
-    implementation(project(":libraries:entityselector"))
-    implementation(project(":libraries:skrrt"))
-//    implementation(project(":libraries:dax-api"))
-}
